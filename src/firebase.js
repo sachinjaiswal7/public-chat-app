17de2412014ff7a11350d769590c18adfdc9102a
sachin <sj62513@gmail.com>
2023-07-29 16:51:39 +0530

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,14 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
+  apiKey: "AIzaSyBgAbO9dQBX77YsR46vk4K7OFaWugvSPKM",
+  authDomain: "chat-app-with-react-d4924.firebaseapp.com",
+  projectId: "chat-app-with-react-d4924",
+  storageBucket: "chat-app-with-react-d4924.appspot.com",
+  messagingSenderId: "1021383247791",
+  appId: "1:1021383247791:web:4166cd834cdddf0e0dd15a"
 };
 
 // Initialize Firebase
